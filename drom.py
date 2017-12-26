7bae02627bc52cb5868dfff44683591aaaad0947
Emanoel Alvez de Queiroz <emanoel@viasoft.com.br>
2017-12-26 16:48:17 -0200

    Initial Commit

---
+++ drom.py
@@ -0,0 +1,26 @@
+"""Terminal application to download ROMs"""
+
+import argparse
+
+
+def main():
+    """Main Function"""
+    parser = argparse.ArgumentParser(
+        description='Find the ROM you want to play and make the download from the terminal!',
+        version='0.0.1', prog="drom")
+    parser.add_argument('romname',
+                        help='the ROM to be searched')
+    parser.add_argument('-p', '--platform', metavar='',
+                        choices=['psx', 'ps2', 'nes', 'snes', 'n64', 'gc'],
+                        help='add platform to the search query')
+
+    args = parser.parse_args()
+    if args.platform:
+        print 'ROM name: {} Platform: {}'.format(args.romname, args.platform)
+        return
+
+    print 'ROM name: {}'.format(args.romname)
+
+
+if __name__ == "__main__":
+    main()
