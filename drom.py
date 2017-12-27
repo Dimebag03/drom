d38d426c60fb8ca88ea8a0e57ad4815b39a9445c
Emanoel Alvez de Queiroz <emanoel@viasoft.com.br>
2017-12-27 11:00:05 -0200

    Added Pipfile and requests lib

--- drom.py
+++ drom.py
@@ -1,19 +1,27 @@
 """Terminal application to download ROMs"""
 
 import argparse
+# import requests
 
 
 def main():
     """Main Function"""
     parser = argparse.ArgumentParser(
         description='Find the ROM you want to play and make the download from the terminal!',
-        version='0.0.1', prog="drom")
+        version='drom v0.0.1', prog="drom")
     parser.add_argument('romname',
                         help='the ROM to be searched')
     parser.add_argument('-p', '--platform', metavar='',
                         choices=['psx', 'ps2', 'nes',
                                  'snes', 'n64', 'gc', 'wii'],
                         help='add platform to the search query')
+
+    # How to use the lib "requests"
+    # response = requests.get('https://api.github.com/user',
+    #                         auth=('user', 'pass'))
+    # json_r = response.json()
+    # print json_r
+    # print json_r['login']
 
     args = parser.parse_args()
     if args.platform:
